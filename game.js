736b9eed47bef447c37e3e00f007726f506f994f
Vladislav S <>
2021-02-24 17:08:58 +0200

    Reworked formatting

--- game.js
+++ game.js
@@ -3,10 +3,9 @@ const roundResults = document.querySelector('#roundResults');
 const scoreBoardP = document.querySelector('#scoreP');
 const scoreBoardC = document.querySelector('#scoreC');
 
-
 // Computer choice //
 
-function getComputerPlay(){
+function getComputerPlay() {
     let random = Math.floor(Math.random()*3);
     switch(random){
         case(0):
@@ -25,14 +24,13 @@ function getComputerPlay(){
 let playerChoice;
 optionButtons.forEach((button)=>{button.addEventListener('click',()=>{
 
-    if (button.id == "0"){
+    if (button.id == "0") {
         playerChoice = 0;
     }
-    else if (button.id == "1"){
+    else if (button.id == "1") {
         playerChoice = 1;
     }
-    else if (button.id == "2")
-    {
+    else if (button.id == "2") {
         playerChoice = 2;
     }
     playGame(playerChoice, getComputerPlay());
@@ -40,36 +38,36 @@ optionButtons.forEach((button)=>{button.addEventListener('click',()=>{
 
 });
 
-
 // plays a round //
+
 let computerScore = 0;
 let playerScore = 0;
 
-function playGame(player, computer){
+function playGame(player, computer) {
     if (player == computer){
         roundResults.textContent = 'Round result: Tie! You both chose the same card.';
     }
-    if (player == 0 && computer == 2){
+    else if (player == 0 && computer == 2) {
         playerScore++;
         roundResults.textContent = 'Round result: Won! You picked Rock, the computer picked Scissors';
     }
-    if (player == 1 && computer == 0){
+    else if (player == 1 && computer == 0) {
         playerScore++;
         roundResults.textContent = 'Round result: Won! You picked Paper, the computer picked Rock';
     }
-    if (player == 2 && computer == 1){
+    else if (player == 2 && computer == 1) {
         playerScore++;
         roundResults.textContent = 'Round result: Won! You picked Scissors, the computer picked Paper';
     }
-    if (player == 2 && computer == 0){
+    else if (player == 2 && computer == 0) {
         computerScore++;
         roundResults.textContent = 'Round result: Lost! You picked Scissors, the computer picked Rock';
     }
-    if (player == 0 && computer == 1){
+    else if (player == 0 && computer == 1) {
         computerScore++;
         roundResults.textContent = 'Round result: Lost! You picked Rock, the computer picked Paper';
     }
-    if (player == 1 && computer == 2){
+    else if (player == 1 && computer == 2) {
         computerScore++;
         roundResults.textContent = 'Round result: Lost! You picked Paper, the computer picked Scissors';
     }
